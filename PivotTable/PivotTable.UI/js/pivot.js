b79e0850bc2c99aa27a94f3dd3eb5c1602c3e50d
ASENTECH\mahendra.pawar <pawarmahen.pawar@gmail.com>
2017-08-02 12:46:34 -0400

    simple pivot table generated

--- PivotTable/PivotTable.UI/js/pivot.js
+++ PivotTable/PivotTable.UI/js/pivot.js
@@ -1,4 +1,23 @@
 ﻿var pivot = (function (jQ) {
+    var pivotModel = {};
+
+    pivotModel.simplePivot = function (data, id) {
+        if (data == undefined || id == undefined) {
+            return 'data or id cannot be empty';
+        }
+        jQ(id).pivot(
+            [
+                { color: "blue", shape: "circle" },
+                { color: "red", shape: "triangle" }
+            ],
+            {
+                rows: ["color"],
+                cols: ["shape"]
+            }
+        );
+    }
+
+    return pivotModel;
 
 })(jQuery);
 
